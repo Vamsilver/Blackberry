65e7c25b5d568b6eead5e592eb3a3126883ae635
Vamsilver <vamsilver145@gmail.com>
2022-10-04 13:12:46 +0300

    Добавьте файлы проекта.

---
+++ Blackberry/ClassApp/DbConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blackberry.ADOApp;
+
+namespace Blackberry.ClassApp
+{
+    public class DbConnection
+    {
+        public static BlackMarketEntities Connection = new BlackMarketEntities();
+    }
+}
